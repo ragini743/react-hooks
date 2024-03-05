a2e53762086ec85091240de7698d257d5a98c794
Ragini <raginiv743@gmail.com>
2024-03-05 20:22:10 +0530

    install tailwind

--- src/component/Demo.js
+++ src/component/Demo.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Demo = () => {
+  const [text,setText] = useState("");
+
   return (
-    <div>Demo</div>
+    <div className='mt-6 ml-6 border-2 border-gray-400 h-72 w-[30%] p-4 '>
+      <label>
+        Name
+      </label>
+      <div className='border-2 border-gray-500 mt-4 rounded-lg'>
+        <input type='text' value={text} onChange={(e) =>setText(e.target.value)} className='rounded-lg outline-none w-full pl-4'>
+
+        </input>
+      </div>
+      
+      </div>
   )
 }
 
